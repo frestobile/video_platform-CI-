99265a9b767cc531b7ddeb4307872ce2d46175d9
frestobile <forever_ourvictory@hotmail.com>
2025-05-09 11:09:17 -0300

    hosting email service

--- application/config/email.php
+++ application/config/email.php
@@ -2,14 +2,6 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-    
-    // $config['protocol']	    = 'smtp';
-    // $config['smtp_port']	= '587';
-    // $config['smtp_host']	= 'smtp.sendgrid.net';
-    // $config['smtp_user']	= 'apikey';
-    // $config['smtp_pass']	= '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';
-    // $config['crlf'] = '\r\n';
-    // $config['newline'] = '\r\n';
 
         $config['protocol']  = getenv('EMAIL_PROTOCOL');
         $config['smtp_host'] = getenv('EMAIL_HOST');
